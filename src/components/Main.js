0f97faed50924f6fb0258bb46409e300070f3d6a
Scott Schermerhorn <plaidscott@gmail.com>
2017-09-03 10:21:54 -0600

    sets up axios requests to use proxy (package.json) port 3001, which is our json-server.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
 import React, { Component} from 'react';
+import api from '../utils/api/api.js';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: ''
+    }
+  }
+  test() {
+    api.test()
+      .then( response => {
+        console.log('response in main.js from test', response);
+      })
+  }
+
   render() {
+    this.test();
     return (
       <div>
         <div>Hello there from Main!</div>
